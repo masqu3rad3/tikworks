30946466deeb1c4d97e8c949d9eedbf96613d7a7
Arda Kutlu <ardakutlu@gmail.com>
2026-01-01 19:01:04 +0000

    mirror mapping added to the polish factory defaults

--- src/tik/tools/polish/config.py
+++ src/tik/tools/polish/config.py
@@ -2,6 +2,12 @@ from tik.shared.user_settings import SettingsManager
 
 FACTORY_DEFAULTS = {
     "additional_library_paths": [],
+    "mirror_mapping": {
+        "L_*": "R_*",
+        "Right *": "Left *",
+        "*_L": "*_R",
+        "* Right": "* Left",
+    },
 }
 
 settings = SettingsManager("tik_polish_settings", FACTORY_DEFAULTS)
